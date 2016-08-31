414d36f60cd4e324650ae1e2490edb7a8bed8a04
x7chen@126.com <x7chen@126.com>
2016-08-31 18:01:38 +0800

    0831-1800

---
+++ app/src/main/java/com/pumelotech/dev/pumelotree/Downloader/Downloader.java
@@ -0,0 +1,7 @@
+package com.pumelotech.dev.pumelotree.Downloader;
+
+/**
+ * Created by Administrator on 2016/8/31.
+ */
+public class Downloader {
+}
---
+++ app/src/main/java/com/pumelotech/dev/pumelotree/Editor/Editor.java
@@ -0,0 +1,7 @@
+package com.pumelotech.dev.pumelotree.Editor;
+
+/**
+ * Created by Administrator on 2016/8/31.
+ */
+public class Editor {
+}

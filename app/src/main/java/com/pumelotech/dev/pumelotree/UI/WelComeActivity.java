315594d43f9c3dc5409a4906b3ca46015238abef
sean <x7chen@126.com>
2016-09-03 00:17:53 +0800

    0903-0000

--- app/src/main/java/com/pumelotech/dev/pumelotree/UI/WelComeActivity.java
+++ app/src/main/java/com/pumelotech/dev/pumelotree/UI/WelComeActivity.java
@@ -4,16 +4,20 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ProgressBar;
 
 import com.pumelotech.dev.pumelotree.MyApplication;
 import com.pumelotech.dev.pumelotree.R;
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Administrator on 2016/9/2.
@@ -24,27 +28,50 @@ public class WelComeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
         taskBackGroud();
-    }
-
-    void taskBackGroud(){
-
-        new AsyncTask<Void,Void,Integer>(){
-
+        AsyncTask<String,Integer,String> asyncTask = new AsyncTask<String, Integer, String>() {
             @Override
-            protected Integer doInBackground(Void... voids) {
-                MyApplication.font = Typeface.createFromAsset(getAssets(), "fonts/simsun.ttc");
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent intent = new Intent(WelComeActivity.this, MainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                },5000);
+            protected String doInBackground(String... params) {
                 return null;
             }
         };
+
+    }
+
+    void taskBackGroud() {
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        new AsyncTask<String, Integer, String>() {
+
+            @Override
+            protected String doInBackground(String... params) {
+                MyApplication.font = Typeface.createFromAsset(getAssets(), "fonts/simsun.ttc");
+                try {
+                    for (int i = 0; i < 21; i++) {
+                        publishProgress(100 * i / 20);
+                        Thread.sleep(50);
+
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(WelComeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return null;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected void onProgressUpdate(Integer... params) {
+                super.onProgressUpdate(params);
+                progressBar.setProgress(params[0]);
+            }
+        }.execute();
     }
 }
